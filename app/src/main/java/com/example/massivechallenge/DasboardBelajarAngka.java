265e96e226a70fc2d0e6774714ba5ec76f4ecb30
AissyaEmbun <aissyaembun.ee78@gmail.com>
2022-06-07 12:59:28 +0700

    tambahan belajar

---
+++ app/src/main/java/com/example/massivechallenge/DasboardBelajarAngka.java
@@ -0,0 +1,61 @@
+package com.example.massivechallenge;
+
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DasboardBelajarAngka extends AppCompatActivity {
+
+    ImageView back;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        setContentView(R.layout.activity_dasboard_belajar_angka);
+
+
+        Animation animation = AnimationUtils.loadAnimation(DasboardBelajarAngka.this,R.anim.splash);
+
+        back = findViewById(R.id.back);
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                back.startAnimation(animation);
+
+                animation.setAnimationListener(new Animation.AnimationListener() {
+                    @Override
+                    public void onAnimationStart(Animation animation) {
+                        MediaPlayer mediaPlayer = MediaPlayer.create(DasboardBelajarAngka.this,R.raw.click_sound_effect);
+                        mediaPlayer.start();
+                    }
+
+                    @Override
+                    public void onAnimationEnd(Animation animation) {
+
+                        Intent intent = new Intent(DasboardBelajarAngka.this,DashboardBelajar.class);
+                        startActivity(intent);
+
+                    }
+
+                    @Override
+                    public void onAnimationRepeat(Animation animation) {
+
+                    }
+                });
+
+            }
+        });
+
+
+
+    }
+}
---
+++ app/src/main/java/com/example/massivechallenge/DasboardBelajarHewan.java
@@ -0,0 +1,60 @@
+package com.example.massivechallenge;
+
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DasboardBelajarHewan extends AppCompatActivity {
+
+    ImageView back;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        setContentView(R.layout.activity_dasboard_belajar_hewan);
+
+
+        Animation animation = AnimationUtils.loadAnimation(DasboardBelajarHewan.this,R.anim.splash);
+
+        back = findViewById(R.id.back);
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                back.startAnimation(animation);
+
+                animation.setAnimationListener(new Animation.AnimationListener() {
+                    @Override
+                    public void onAnimationStart(Animation animation) {
+                        MediaPlayer mediaPlayer = MediaPlayer.create(DasboardBelajarHewan.this,R.raw.click_sound_effect);
+                        mediaPlayer.start();
+                    }
+
+                    @Override
+                    public void onAnimationEnd(Animation animation) {
+
+                        Intent intent = new Intent(DasboardBelajarHewan.this,DashboardBelajar.class);
+                        startActivity(intent);
+
+                    }
+
+                    @Override
+                    public void onAnimationRepeat(Animation animation) {
+
+                    }
+                });
+
+            }
+        });
+
+
+    }
+}

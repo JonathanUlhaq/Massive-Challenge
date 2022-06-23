3e0a2f398d600fd82ffb09d3ac188a451a64d0d1
alangmulya <alangmulya11@gmail.com>
2022-06-23 12:57:28 +0700

    Edit Dashboard (Penambahan ID), Inisialisasi Manifest, Penambahan String Hewan & Sayur Indo Inggris

--- app/src/main/java/com/example/massivechallenge/DasboardBelajarHewan.java
+++ app/src/main/java/com/example/massivechallenge/DasboardBelajarHewan.java
@@ -1,13 +1,18 @@
 package com.example.massivechallenge;
 
+import android.app.Dialog;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,6 +20,7 @@ import androidx.appcompat.app.AppCompatActivity;
 public class DasboardBelajarHewan extends AppCompatActivity {
 
     ImageView back;
+    FrameLayout belajar_hewan,tebak_hewan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,6 +47,63 @@ public class DasboardBelajarHewan extends AppCompatActivity {
         Animation animation = AnimationUtils.loadAnimation(DasboardBelajarHewan.this,R.anim.splash);
 
         back = findViewById(R.id.back);
+        belajar_hewan = findViewById(R.id.belajar_hewan);
+        tebak_hewan = findViewById(R.id.tebak_hewan);
+
+        belajar_hewan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                belajar_hewan.startAnimation(animation);
+
+                animation.setAnimationListener(new Animation.AnimationListener() {
+                    @Override
+                    public void onAnimationStart(Animation animation) {
+                        MediaPlayer mediaPlayer = MediaPlayer.create(getApplicationContext(),R.raw.click_sound_effect);
+                        mediaPlayer.start();
+                        Intent intent = new Intent(getApplicationContext(),IsiBelajarHewan.class);
+                        startActivity(intent);
+                    }
+
+                    @Override
+                    public void onAnimationEnd(Animation animation) {
+
+
+
+                    }
+
+                    @Override
+                    public void onAnimationRepeat(Animation animation) {
+
+                    }
+                });
+            }
+        });
+
+        tebak_hewan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tebak_hewan.startAnimation(animation);
+                animation.setAnimationListener(new Animation.AnimationListener() {
+                    @Override
+                    public void onAnimationStart(Animation animation) {
+                        MediaPlayer mediaPlayer = MediaPlayer.create(DasboardBelajarHewan.this,R.raw.click_sound_effect);
+                        mediaPlayer.start();
+                        showDialog();
+                    }
+
+                    @Override
+                    public void onAnimationEnd(Animation animation) {
+
+                    }
+
+                    @Override
+                    public void onAnimationRepeat(Animation animation) {
+
+                    }
+                });
+
+            }
+        });
 
         back.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -55,11 +118,11 @@ public class DasboardBelajarHewan extends AppCompatActivity {
 
                         Intent intent = new Intent(DasboardBelajarHewan.this,DashboardBelajar.class);
                         startActivity(intent);
-
                     }
 
                     @Override
                     public void onAnimationEnd(Animation animation) {
+
 
 
                     }
@@ -72,7 +135,78 @@ public class DasboardBelajarHewan extends AppCompatActivity {
 
             }
         });
+    }
 
+    public void showDialog()
+    {
+        final Dialog dialog = new Dialog(DasboardBelajarHewan.this);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+
+        dialog.setCancelable(true);
+
+        dialog.setContentView(R.layout.dialog_bahasa);
+        Animation animation = AnimationUtils.loadAnimation(DasboardBelajarHewan.this,R.anim.splash);
+
+        final ImageView bahasa_indonesia = dialog.findViewById(R.id.bahasa_indonesia);
+        final ImageView bahasa_inggris = dialog.findViewById(R.id.bahasa_inggris);
+
+        bahasa_indonesia.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                bahasa_indonesia.startAnimation(animation);
+
+                animation.setAnimationListener(new Animation.AnimationListener() {
+                    @Override
+                    public void onAnimationStart(Animation animation) {
+                        Intent intent = new Intent(getApplicationContext(),TebakHewanIndonesia.class);
+                        startActivity(intent);
+                    }
+
+                    @Override
+                    public void onAnimationEnd(Animation animation) {
+
+                    }
+
+                    @Override
+                    public void onAnimationRepeat(Animation animation) {
+
+                    }
+                });
+
+            }
+        });
+
+
+        bahasa_inggris.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                bahasa_inggris.startAnimation(animation);
+
+                animation.setAnimationListener(new Animation.AnimationListener() {
+                    @Override
+                    public void onAnimationStart(Animation animation) {
+                        Intent intent = new Intent(getApplicationContext(),TebakHewanInggris.class);
+                        startActivity(intent);
+                    }
+
+                    @Override
+                    public void onAnimationEnd(Animation animation) {
+
+                    }
+
+                    @Override
+                    public void onAnimationRepeat(Animation animation) {
+
+                    }
+                });
+
+            }
+        });
+
+        dialog.show();
 
     }
 }
--- app/src/main/java/com/example/massivechallenge/DasboardBelajarSayurs.java
+++ app/src/main/java/com/example/massivechallenge/DasboardBelajarSayurs.java
@@ -1,13 +1,18 @@
 package com.example.massivechallenge;
 
+import android.app.Dialog;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,7 +21,7 @@ import androidx.appcompat.app.AppCompatActivity;
 public class DasboardBelajarSayurs extends AppCompatActivity {
 
     ImageView back;
-
+    FrameLayout belajar_sayur,tebak_sayur;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,9 +44,67 @@ public class DasboardBelajarSayurs extends AppCompatActivity {
                 | View.SYSTEM_UI_FLAG_FULLSCREEN;
         decorView.setSystemUiVisibility(uiOptions);
 
+
         Animation animation = AnimationUtils.loadAnimation(DasboardBelajarSayurs.this,R.anim.splash);
 
         back = findViewById(R.id.back);
+        belajar_sayur = findViewById(R.id.belajar_sayur);
+        tebak_sayur = findViewById(R.id.tebak_sayur);
+
+        belajar_sayur.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                belajar_sayur.startAnimation(animation);
+
+                animation.setAnimationListener(new Animation.AnimationListener() {
+                    @Override
+                    public void onAnimationStart(Animation animation) {
+                        MediaPlayer mediaPlayer = MediaPlayer.create(getApplicationContext(),R.raw.click_sound_effect);
+                        mediaPlayer.start();
+                        Intent intent = new Intent(getApplicationContext(),IsiBelajarSayur.class);
+                        startActivity(intent);
+                    }
+
+                    @Override
+                    public void onAnimationEnd(Animation animation) {
+
+
+
+                    }
+
+                    @Override
+                    public void onAnimationRepeat(Animation animation) {
+
+                    }
+                });
+            }
+        });
+
+        tebak_sayur.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tebak_sayur.startAnimation(animation);
+                animation.setAnimationListener(new Animation.AnimationListener() {
+                    @Override
+                    public void onAnimationStart(Animation animation) {
+                        MediaPlayer mediaPlayer = MediaPlayer.create(DasboardBelajarSayurs.this,R.raw.click_sound_effect);
+                        mediaPlayer.start();
+                        showDialog();
+                    }
+
+                    @Override
+                    public void onAnimationEnd(Animation animation) {
+
+                    }
+
+                    @Override
+                    public void onAnimationRepeat(Animation animation) {
+
+                    }
+                });
+
+            }
+        });
 
         back.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -54,7 +117,6 @@ public class DasboardBelajarSayurs extends AppCompatActivity {
                         MediaPlayer mediaPlayer = MediaPlayer.create(DasboardBelajarSayurs.this,R.raw.click_sound_effect);
                         mediaPlayer.start();
 
-
                         Intent intent = new Intent(DasboardBelajarSayurs.this,DashboardBelajar.class);
                         startActivity(intent);
                     }
@@ -62,6 +124,49 @@ public class DasboardBelajarSayurs extends AppCompatActivity {
                     @Override
                     public void onAnimationEnd(Animation animation) {
 
+
+
+                    }
+
+                    @Override
+                    public void onAnimationRepeat(Animation animation) {
+
+                    }
+                });
+
+            }
+        });
+    }
+
+    public void showDialog()
+    {
+        final Dialog dialog = new Dialog(DasboardBelajarSayurs.this);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+
+        dialog.setCancelable(true);
+
+        dialog.setContentView(R.layout.dialog_bahasa);
+        Animation animation = AnimationUtils.loadAnimation(DasboardBelajarSayurs.this,R.anim.splash);
+
+        final ImageView bahasa_indonesia = dialog.findViewById(R.id.bahasa_indonesia);
+        final ImageView bahasa_inggris = dialog.findViewById(R.id.bahasa_inggris);
+
+        bahasa_indonesia.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                bahasa_indonesia.startAnimation(animation);
+
+                animation.setAnimationListener(new Animation.AnimationListener() {
+                    @Override
+                    public void onAnimationStart(Animation animation) {
+                        Intent intent = new Intent(getApplicationContext(),TebakSayurIndonesia.class);
+                        startActivity(intent);
+                    }
+
+                    @Override
+                    public void onAnimationEnd(Animation animation) {
 
                     }
 
@@ -75,7 +180,34 @@ public class DasboardBelajarSayurs extends AppCompatActivity {
         });
 
 
+        bahasa_inggris.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
+                bahasa_inggris.startAnimation(animation);
+
+                animation.setAnimationListener(new Animation.AnimationListener() {
+                    @Override
+                    public void onAnimationStart(Animation animation) {
+                        Intent intent = new Intent(getApplicationContext(),TebakSayurInggris.class);
+                        startActivity(intent);
+                    }
+
+                    @Override
+                    public void onAnimationEnd(Animation animation) {
+
+                    }
+
+                    @Override
+                    public void onAnimationRepeat(Animation animation) {
+
+                    }
+                });
+
+            }
+        });
+
+        dialog.show();
 
     }
 

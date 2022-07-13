75c3612c2f7c6375d9c9b59639912550c1e7b34d
Jovian Nathan <vianulhag@gmail.com>
2022-07-13 22:13:32 +0700

    Menulis

---
+++ app/src/main/java/com/example/massivechallenge/BelajarMenulisBesar.java
@@ -0,0 +1,14 @@
+package com.example.massivechallenge;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BelajarMenulisBesar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_belajar_menulis_besar);
+    }
+}
--- app/src/main/java/com/example/massivechallenge/DasboardBelajarMenulis.java
+++ app/src/main/java/com/example/massivechallenge/DasboardBelajarMenulis.java
@@ -2,19 +2,28 @@ package com.example.massivechallenge;
 
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.graphics.Color;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.danny_jiang.tracinglibrary.bean.LetterFactory;
+import com.danny_jiang.tracinglibrary.view.TracingLetterView;
 
 public class DasboardBelajarMenulis extends AppCompatActivity {
 
     ImageView back;
+    FrameLayout huruf_besar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +31,8 @@ public class DasboardBelajarMenulis extends AppCompatActivity {
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_dasboard_belajar_menulis);
+
+
 
         View decorView = getWindow().getDecorView();
 
@@ -38,6 +49,44 @@ public class DasboardBelajarMenulis extends AppCompatActivity {
         decorView.setSystemUiVisibility(uiOptions);
 
         Animation animation = AnimationUtils.loadAnimation(DasboardBelajarMenulis.this,R.anim.splash);
+
+
+        huruf_besar = findViewById(R.id.huruf_besar);
+        huruf_besar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                huruf_besar.startAnimation(animation);
+
+                animation.setAnimationListener(new Animation.AnimationListener() {
+                    @Override
+                    public void onAnimationStart(Animation animation) {
+
+                        MediaPlayer mediaPlayer = MediaPlayer.create(DasboardBelajarMenulis.this,R.raw.click_sound_effect);
+                        mediaPlayer.start();
+                        mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                            @Override
+                            public void onCompletion(MediaPlayer mediaPlayer) {
+                                mediaPlayer.reset();
+                            }
+                        });
+                        Intent intent = new Intent(DasboardBelajarMenulis.this,BelajarMenulisBesar.class);
+                        startActivity(intent);
+
+                    }
+
+                    @Override
+                    public void onAnimationEnd(Animation animation) {
+
+                    }
+
+                    @Override
+                    public void onAnimationRepeat(Animation animation) {
+
+                    }
+                });
+
+            }
+        });
 
         back = findViewById(R.id.back);
 
